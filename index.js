0655f3395518c8f7f67490122e28477e58fc0aea
Jannis R <mail@jannisr.de>
2022-05-10 16:12:05 +0200

    Agent ✨

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-// todo
+import createAgent from 'agent-base'
+import {connect as netConnect} from 'net'
+import {connect as tlsConnect} from 'tls'
+import {createIpPool} from './lib/ip-pool.js'
+
+const SECOND = 1000
+const MINUTE = 60 * SECOND
+
+// todo: is there a way to determine the default network interface?
+
+const createIpPoolLocalAddressAgent = async (ipAddresses, iface, opt = {}) => {
+	const poolCfg = {
+		useExistingAddresses: true,
+		addressAssignTimeout: SECOND,
+		addressRemoveTimeout: SECOND,
+		addressMaxIdleTime: 10 * MINUTE,
+		removeAddressesEvery: 10 * SECOND,
+		...opt,
+	}
+	const ipPool = await createIpPool(ipAddresses, iface, poolCfg)
+
+	const createSocketWithLocalAddressFromIpPool = async (req, options) => {
+		const localAddress = await ipPool.acquire().promise
+		options = {
+			...options,
+			localAddress,
+		}
+
+		const socket = options.secureEndpoint
+			? tlsConnect(options)
+			: netConnect(options)
+		// todo: release address after request is done!
+
+		return socket
+	}
+
+	return createAgent(createSocketWithLocalAddressFromIpPool)
+}
 
 export {
-	// todo
+	createIpPoolLocalAddressAgent as createIpPoolAgent,
 }

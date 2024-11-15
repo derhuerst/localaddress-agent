391b185b1349c4b8598d06b9d32df480b7262f5f
Jannis R <mail@jannisr.de>
2024-11-15 23:02:28 +0100

    random-from-env.js: unassign/delete addresses on exit

--- random-from-env.js
+++ random-from-env.js
@@ -1,6 +1,7 @@
 import createDebug from 'debug'
 import {createRandomAddressesGenerator} from './lib/random-addresses-from-range.js'
 import {createIpPoolAgent} from './index.js'
+import {withSoftExit} from './lib/soft-exit.js'
 
 const debug = createDebug('localaddress-agent:random-from-env')
 
@@ -9,6 +10,7 @@ let randomLocalAddressAgent = null
 if (process.env.RANDOM_LOCAL_ADDRESSES_RANGE) {
 	const range = process.env.RANDOM_LOCAL_ADDRESSES_RANGE
 	const iface = process.env.RANDOM_LOCAL_ADDRESSES_INTERFACE || 'eth0'
+	const deleteOnExit = process.env.RANDOM_LOCAL_ADDRESSES_DELETE_ON_EXIT !== 'false'
 
 	debug(`creating an HTTP Agent that assigns random addresses from ${range} to ${iface}`)
 	const randomAddresses = createRandomAddressesGenerator(range)()
@@ -16,6 +18,16 @@ if (process.env.RANDOM_LOCAL_ADDRESSES_RANGE) {
 		randomAddresses,
 		iface,
 	)
+
+	if (deleteOnExit) {
+		withSoftExit(() => {
+			randomLocalAddressAgent.destroy()
+			.catch((err) => {
+				console.error(err)
+				process.exit()
+			})
+		})
+	}
 } else {
 	debug('not creating an HTTP Agent because $RANDOM_LOCAL_ADDRESSES_RANGE is missing/empty')
 }
